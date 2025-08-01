e36951de831cd640a388b58b8abad4fe4dad3ec0
Dung-glitch <dungltpn00057@gmail.com>
2025-08-01 17:22:54 +0700

    feat(views): giao diện admin
    -thay đổi mật khẩu
    -cập nhật thông tin cá nhân

---
+++ resources/views/admin/profile/change-password.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+@section('content')
+<section section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xl-12 col-lg-12 col-md-12">
+        <div class="card card-default">
+          <div class="card-header">
+            <h3 class="card-title">Thay đổi mật khẩu cá nhân</h3>
+            <div class="card-tools">
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <x-form-crud 
+            route="{{ route('admin.profile_update-password') }}"
+            :fields="$fields"
+            :rules="$rules"
+            :messages="$messages"
+            textSubmit="Cập Nhật"
+            cancelBtn="false"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /.container-fluid -->
+</section>
+@endsection
