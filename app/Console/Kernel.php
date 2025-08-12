5fc5b01a268c8d2d8d51151989872f79845d824f
thangnv05 <thangnvpn00120@gmail.com>
2025-08-12 23:27:40 +0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,32 +1,73 @@
 <?php
-
-namespace App\Console;
-
-use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
-class Kernel extends ConsoleKernel
-{
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
+if (!function_exists('sortString')) {
+    function sortString($string = '')
     {
-        // $schedule->command('inspire')->hourly();
+        $extract = explode(',', $string);
+        // loại bỏ khoản trắng quanh phaan tử
+        $extract = array_map('trim', $extract);
+        sort($extract, SORT_NUMERIC);
+        $newArray = implode(',', $extract);
+        return $newArray;
     }
-
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
+}
+if (!function_exists('sortAttributeId')) {
+    function sortAttributeId(array $attribute_id = [])
     {
-        $this->load(__DIR__.'/Commands');
+        $attributeId = array_map('trim', $attribute_id);
+        sort($attributeId, SORT_NUMERIC);
+        $attributeId = implode(',', $attributeId);
+        return $attributeId;
+    }
+}
 
-        require base_path('routes/console.php');
+// vnpay
+if (!function_exists('vnpayConfig')) {
+    function vnpayConfig()
+    {
+        return [
+            'vnp_Url' => 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
+            'vnp_Returnurl' => config('app.url') . '/return/vnpay',
+            'vnp_TmnCode' => '1C5MASGJ',
+            'vnp_HashSecret' => '05GRXUNICKIUM8NHTF6GL73GXPIVQTX0',
+            'vnp_apiUrl' => 'https://sandbox.vnpayment.vn/merchant_webapi/merchant.html',
+            'apiUrl' => 'https://sandbox.vnpayment.vn/merchant_webapi/api/transaction'
+        ];
+    }
+}
+
+// momo
+if (!function_exists('momoConfig')) {
+    function momoConfig()
+    {
+        return [
+            'partnerCode' => 'MOMOBKUN20180529',
+            'accessKey' => 'klm05TvNBzhg7h7j',
+            'secretKey' => 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa',
+        ];
+    }
+}
+if (!function_exists('execPostRequest')) {
+    function execPostRequest($url, $data)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data)
+            )
+        );
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        //execute post
+        $result = curl_exec($ch);
+
+        //close connection
+        curl_close($ch);
+        return $result;
     }
 }
